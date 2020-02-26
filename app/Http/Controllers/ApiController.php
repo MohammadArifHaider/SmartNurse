a2023adad0e101d27b46734fe91bf6dba67e524f
n0k1b <nokibevon7@gmail.com>
2020-02-26 14:18:58 +0600

    update at 26-20-2020 at 2:18 PM

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -333,37 +333,37 @@ curl_close($curl);
          $date = date('d-m-Y');
          return $date;
 
-        //  $date2 = explode("-",$date);
-        //  $month = $date2[1];
-        //  if(strlen($month)==1)
-        //  {
-        //      $month ="0".$month;
-        //  }
-        //  $date = $date2[0]."-".$month."-".$date2[2];
-        // $nurse_id = $request->user_id;
-        // $appointment = nurse_scheduler::where('nurse_id','=',$nurse_id)->where('appointed_date','=',$date)->where('cancle','=','no')->get();
-        // $appointment_list = array();
+         $date2 = explode("-",$date);
+         $month = $date2[1];
+         if(strlen($month)==1)
+         {
+             $month ="0".$month;
+         }
+         $date = $date2[0]."-".$month."-".$date2[2];
+        $nurse_id = $request->user_id;
+        $appointment = nurse_scheduler::where('nurse_id','=',$nurse_id)->where('appointed_date','=',$date)->where('cancle','=','no')->get();
+        $appointment_list = array();
 
-        // if($appointment->isEmpty())
-        // {
-        //    // return $date;
-        //     return json_encode($appointment_list);
-        // }
-        // else
-        // {
-        //     for($i = 0 ;$i<sizeof($appointment);$i++)
-        //     {
-        //         $patient_id = $appointment[$i]->patient_id;
-        //         $patient = patient_profile::where('id','=',$patient_id)->first();
-        //         $patient_name = $patient->first_name." ".$patient->last_name;
-        //         $address = $patient->address.",".$patient->city.",".$patient->country;
-        //         $patient_lat = distance_table::where('patient_id','=',$patient_id)->first()->patient_lat;
-        //         $patient_lon = distance_table::where('patient_id','=',$patient_id)->first()->patient_lon;
+        if($appointment->isEmpty())
+        {
+           // return $date;
+            return json_encode($appointment_list);
+        }
+        else
+        {
+            for($i = 0 ;$i<sizeof($appointment);$i++)
+            {
+                $patient_id = $appointment[$i]->patient_id;
+                $patient = patient_profile::where('id','=',$patient_id)->first();
+                $patient_name = $patient->first_name." ".$patient->last_name;
+                $address = $patient->address.",".$patient->city.",".$patient->country;
+                $patient_lat = distance_table::where('patient_id','=',$patient_id)->first()->patient_lat;
+                $patient_lon = distance_table::where('patient_id','=',$patient_id)->first()->patient_lon;
 
-        //         array_push($appointment_list,['scheduler_id'=>$appointment[$i]->id,'p_name'=>$patient_name,'p_id'=>$patient_id,'appointment_time'=>$appointment[$i]->appointed_start_time,'lat'=>$patient_lat,'lon'=>$patient_lon,'p_address'=>$address]);
-        //     }
-        //     return json_encode($appointment_list);
-        // }
+                array_push($appointment_list,['scheduler_id'=>$appointment[$i]->id,'p_name'=>$patient_name,'p_id'=>$patient_id,'appointment_time'=>$appointment[$i]->appointed_start_time,'lat'=>$patient_lat,'lon'=>$patient_lon,'p_address'=>$address]);
+            }
+            return json_encode($appointment_list);
+        }
 
 
     }
